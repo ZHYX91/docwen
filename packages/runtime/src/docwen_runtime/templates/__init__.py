b6eae0b430e90b4b75effcd3db30ad609b0be49b
ZHYX91 <80815497+ZHYX91@users.noreply.github.com>
2026-09-13 20:52:20 +0800

    feat: expose template management API

--- packages/runtime/src/docwen_runtime/templates/__init__.py
+++ packages/runtime/src/docwen_runtime/templates/__init__.py
@@ -1,5 +1,6 @@
-"""Template discovery registry."""
+"""Template discovery and user-management APIs."""
 
+from docwen_runtime.templates.manager import TemplateManagementError, TemplateManager
 from docwen_runtime.templates.registry import (
     TemplateIdentityConflictError,
     TemplateInfo,
@@ -9,13 +10,19 @@ from docwen_runtime.templates.registry import (
     is_canonical_template_id,
     validate_template_path,
 )
+from docwen_runtime.templates.state import TemplateStateStore, template_state_path, user_templates_dir
 
 __all__ = [
     "TemplateIdentityConflictError",
     "TemplateInfo",
+    "TemplateManagementError",
+    "TemplateManager",
     "TemplateNotFoundError",
     "TemplateRegistry",
     "TemplateResolutionError",
+    "TemplateStateStore",
     "is_canonical_template_id",
+    "template_state_path",
+    "user_templates_dir",
     "validate_template_path",
 ]
